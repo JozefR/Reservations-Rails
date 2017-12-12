a4c18d39f9ec2b1700310e0a6ee14cb45398ed39
Jozef Randjak <randjakjozef@gmail.com>
2017-12-12 18:50:47 +0100

    add room scaffold

---
+++ app/models/room.rb
@@ -0,0 +1,13 @@
+# == Schema Information
+#
+# Table name: rooms
+#
+#  id         :integer          not null, primary key
+#  title      :string
+#  code       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class Room < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171212142230) do
+ActiveRecord::Schema.define(version: 20171212173021) do
 
   create_table "buildings", force: :cascade do |t|
+    t.string "title"
+    t.string "code"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "rooms", force: :cascade do |t|
     t.string "title"
     t.string "code"
     t.datetime "created_at", null: false
