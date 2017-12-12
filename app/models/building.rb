641ae634f91c2a7f66765d91fcc979d450ebcd31
Jozef Randjak <randjakjozef@gmail.com>
2017-12-12 17:36:42 +0100

    add annotate and validation to buildings

--- app/models/building.rb
+++ app/models/building.rb
@@ -1,2 +1,16 @@
+# == Schema Information
+#
+# Table name: buildings
+#
+#  id         :integer          not null, primary key
+#  title      :string
+#  code       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Building < ApplicationRecord
+
+  validates :title, presence: true, length: {minimum: 3, maximum: 30}
+  validates :code, presence: true, length: {minimum: 2, maximum: 20}
 end
