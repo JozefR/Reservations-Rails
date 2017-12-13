a36e2bd0e4b6f7aadeae1c7f141a7d16eececf57
Jozef Randjak <randjakjozef@gmail.com>
2017-12-13 14:57:48 +0100

    add student model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171213135347) do
+ActiveRecord::Schema.define(version: 20171213135708) do
 
   create_table "buildings", force: :cascade do |t|
     t.string "title"
@@ -47,6 +47,14 @@ ActiveRecord::Schema.define(version: 20171213135347) do
     t.datetime "updated_at", null: false
     t.integer "building_id"
     t.index ["building_id"], name: "index_rooms_on_building_id"
+  end
+
+  create_table "students", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "teachers", force: :cascade do |t|
